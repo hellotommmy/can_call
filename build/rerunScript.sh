6f643414c01289f2d90cb14a3b363a8dbf70d0c0
Ubuntu <ubuntu@ip-172-31-21-80.ec2.internal>
2022-08-07 13:24:30 +0000

    build script

---
+++ build/rerunScript.sh
@@ -0,0 +1,10 @@
+export PATH="~/llvm-project/build/bin:${PATH}"
+#clang -O0 -emit-llvm ~/llvm-project/llvm/lib/Transforms/Hello/hi.c -c -o ~/llvm-project/llvm/lib/Transforms/Hello/hi.bc
+cmake --build .
+./bin/clang -O0 -disable-llvm-passes -disable-llvm-optzns hi.c
+./bin/clang -O1 -disable-llvm-passes -disable-llvm-optzns hi.c
+./bin/clang -O2 -disable-llvm-passes -disable-llvm-optzns hi.c
+./bin/clang -O3 -disable-llvm-passes -disable-llvm-optzns hi.c
+#bin/opt -enable-new-pm=0 -load lib/LLVMHello.so -passes=-called-value-propagation1 < ~/llvm-project/llvm/lib/Transforms/Hello/hi.bc  > /dev/null 
+#bin/opt -enable-new-pm=0 -load lib/LLVMHello.so -passes=-called-value-propagation1 < ~/llvm-project/llvm/lib/Transforms/Hello/hi.bc  > /dev/null 
+#bin/opt -enable-new-pm=0 -load lib/LLVMHello.so -passes=-called-value-propagation1 < ~/llvm-project/llvm/lib/Transforms/Hello/hi.bc  > /dev/null 
