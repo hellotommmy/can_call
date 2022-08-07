4cea5d04abc1edae5567ba0b4c47bca594054478
hellotommmy <1728422631@qq.com>
2022-08-07 22:17:57 +0100

    Update rerunScript.sh

--- build/rerunScript.sh
+++ build/rerunScript.sh
@@ -1,6 +1,6 @@
 export PATH="~/llvm-project/build/bin:${PATH}"
 #clang -O0 -emit-llvm ~/llvm-project/llvm/lib/Transforms/Hello/hi.c -c -o ~/llvm-project/llvm/lib/Transforms/Hello/hi.bc
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DLLVM_USE_LINKER=gold ../llvm
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DLLVM_USE_LINKER=gold ../llvm -DLLVM_ENABLE_PROJECTS="clang"
 cmake --build .
 ./bin/clang -O0 -disable-llvm-passes -disable-llvm-optzns hi.c
 ./bin/clang -O1 -disable-llvm-passes -disable-llvm-optzns hi.c
